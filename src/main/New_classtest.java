ed8171b3dddf94016701bf0585fd50ffa1957773
Urmila Suryawanshi <urmilasuryawanshi195@gmail.com>
2024-04-03 15:33:30 +0530

    ky taku

---
+++ src/main/New_classtest.java
@@ -0,0 +1,12 @@
+package main;
+
+public class New_classtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("New TC for Push");
+	}
+
+}
+//atta  kel
