10e6d4c92c39f96e5431b197d07a7f2b5d42f261
Urmila Suryawanshi <urmilasuryawanshi195@gmail.com>
2024-04-03 15:42:50 +0530

    Successfully launched chrome driver

---
+++ src/main/Cromedriverclass.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Cromedriverclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Launching chrome driver");
+	}
+
+}
